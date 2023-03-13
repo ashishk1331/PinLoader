cf7dee3ea25e0cbc2ee5ab945d1b70724e8725f5
Ashish Khare <akstyles211@gmail.com>
2023-03-13 23:12:33 +0530

    API improvments

--- server/index.js
+++ server/index.js
@@ -5,8 +5,13 @@ import fetchData from './scrape.js'
 const app = express()
 app.use(express.json());
 
+app.get('/', (req, res) => {
+	res.send('API is working now,..')
+})
+
 app.post('/api', async (req, res) => {
 	const { url } = req.body;
+	console.log(url)
 	let r = await fetchData(url)
 	await res.send(r)
 })
